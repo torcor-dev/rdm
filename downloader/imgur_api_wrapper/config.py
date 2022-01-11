9b45939d256a610e4b605675fbfc8ea6c25ca2ec
Tormod Fossum <fossum@pm.me>
2022-01-11 19:11:05 +0100

    Package

--- downloader/imgur_api_wrapper/config.py
+++
@@ -1,9 +0,0 @@
-import yaml
-import os
-
-credential_file = os.path.join(os.environ["SECRETS"], "imgur.yaml")
-with open(credential_file, "r") as f:
-    SECRETS = yaml.load(f, Loader=yaml.Loader)
-
-CLIENT_ID = SECRETS["client_id"]
-CLIENT_SECRET = SECRETS["client_secret"]
